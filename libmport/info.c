9549055861297950059255d87326c8d1867f5274
Lucas Holt <luke@foolishgames.com>
2021-10-12 13:49:09 -0400

    Add more information to info

--- libmport/info.c
+++ libmport/info.c
@@ -26,12 +26,12 @@
 
 #include "mport.h"
 #include "mport_private.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <err.h>
 #include <unistd.h>
-
 
 MPORT_PUBLIC_API char *
 mport_info(mportInstance *mport, const char *packageName)
@@ -45,6 +45,8 @@ mport_info(mportInstance *mport, const char *packageName)
 	int no_shlib_provided = 0;
 	char *info_text = NULL;
 	time_t expirationDate;
+    char *options;
+    char *desc;
 
 	if (mport == NULL) {
 		SET_ERROR(MPORT_ERR_FATAL, "mport not initialized");
@@ -77,6 +79,8 @@ mport_info(mportInstance *mport, const char *packageName)
 		flavor = strdup("N/A");
 		deprecated = strdup("N/A");
 		expirationDate = 0;
+        options = strdup("");
+        desc = strdup("");
 	} else {
 		status = (*packs)->version;
 		origin = (*packs)->origin;
@@ -93,14 +97,22 @@ mport_info(mportInstance *mport, const char *packageName)
 			deprecated = strdup("no");
 		}
 		expirationDate = (*packs)->expiration_date;
+        options = (*packs)->options;
+        if (options == NULL) {
+            options = strdup("");
+        }
+        desc = (*packs)->desc;
+        if (desc == NULL) {
+            options = strdup("");
+        }
 	}
 
 	asprintf(&info_text,
 		 "%s-%s\n"
-			 "Name            : %s\nVersion         : %s\nLatest          : %s\nLicense         : %s\nOrigin          : %s\n"
+			 "Name            : %s\nVersion         : %s\nLatest          : %s\nLicenses         : %s\nOrigin          : %s\n"
 			 "Flavor          : %s\nOS              : %s\n"
 	                 "CPE             : %s\nLocked          : %s\nShared library  : %s\nDeprecated      : %s\nExpiration Date : %s\n"
-			 "\n%s\n",
+			 "Comment: %s\nOptions: %s\nDescription:\n%s\n",
 		 (*indexEntry)->pkgname, (*indexEntry)->version,
 		 (*indexEntry)->pkgname,
 		 status,
@@ -114,7 +126,9 @@ mport_info(mportInstance *mport, const char *packageName)
 		 no_shlib_provided ? "yes" : "no",
 		 deprecated,
 		 expirationDate == 0 ? "" : ctime(&expirationDate),
-		 (*indexEntry)->comment);
+		 (*indexEntry)->comment,
+         options,
+         desc);
 
 	if (packs == NULL) {
 		free(status);
@@ -123,6 +137,8 @@ mport_info(mportInstance *mport, const char *packageName)
 		free(cpe);
 		free(flavor);
 		free(deprecated);
+        free(options);
+        free(desc);
 	} else {
 		mport_pkgmeta_vec_free(packs);
 	}
