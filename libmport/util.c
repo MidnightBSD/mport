07dd5613aa2d5aa97a06b34ad7ecbc873d707523
Lucas Holt <luke@foolishgames.com>
2022-10-16 10:51:37 -0400

    above

--- libmport/util.c
+++ libmport/util.c
@@ -437,6 +437,7 @@ mport_run_asset_exec(mportInstance *mport, const char *fmt, const char *cwd, con
     char *cmnd;
     char *pos;
     char *name;
+    char *lfcpy;
     int ret;
     static int max = 0;
     size_t maxlen = sizeof(max);
@@ -468,7 +469,7 @@ mport_run_asset_exec(mportInstance *mport, const char *fmt, const char *cwd, con
                     max -= l;
                     break;
                 case 'B':
-                    char *lfcpy = malloc(strlen(last_file) * size_of(char));
+                    lfcpy = malloc(strlen(last_file) * size_of(char));
                     name = dirname(lfcpy); /* dirname(3) in MidnightBSD 3.0 and higher modifies the source. */
                     (void) strlcpy(pos, name, max);
                     l = strlen(name);
