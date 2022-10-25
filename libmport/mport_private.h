5355cf8b5ffb026b1feeb6021f41cb745380e8fb
Lucas Holt <luke@foolishgames.com>
2022-10-25 14:44:54 -0400

    rename repo autoupdate to follow mport conventions.
    add examples to man page

--- libmport/mport_private.h
+++ libmport/mport_private.h
@@ -213,7 +213,7 @@ int mport_index_get_mirror_list(mportInstance *, char ***, int *);
 #define MPORT_DAY 3600 * 24
 #define MPORT_MAX_INDEX_AGE MPORT_DAY * 7 /* one week */
 #define MPORT_SETTING_INDEX_LAST_CHECKED "index_last_check"
-#define MPORT_SETTING_REPO_AUTOUPDATE "REPO_AUTOUPDATE"
+#define MPORT_SETTING_REPO_AUTOUPDATE "index_autoupdate"
 
 /* Binaries we use */
 #define MPORT_MTREE_BIN		"/usr/sbin/mtree"
