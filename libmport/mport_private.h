fcfe21bf4630c4b6195a0967a410f75140291aa5
Lucas Holt <luke@foolishgames.com>
2025-03-29 14:47:29 -0400

    bump version

--- libmport/mport_private.h
+++ libmport/mport_private.h
@@ -61,7 +61,7 @@ struct ohash {
 #define MPORT_MASTER_VERSION 13
 #define MPORT_BUNDLE_VERSION 6
 #define MPORT_BUNDLE_VERSION_STR "6"
-#define MPORT_VERSION "2.7.1"
+#define MPORT_VERSION "2.7.2"
 
 #define MPORT_SETTING_MIRROR_REGION "mirror_region"
 #define MPORT_SETTING_TARGET_OS "target_os"
