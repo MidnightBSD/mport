559482991b9d0e1e76dae53a07271449fa1d4949
Lucas Holt <luke@foolishgames.com>
2026-01-16 09:29:48 -0500

    bump version

--- libmport/mport_private.h
+++ libmport/mport_private.h
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 2011, 2013, 2015, 2021-2024 Lucas Holt
+ * Copyright (c) 2011, 2013, 2015, 2021-2026 Lucas Holt
  * Copyright (c) 2007-2009 Chris Reinhardt
  * All rights reserved.
  *
@@ -61,7 +61,7 @@ struct ohash {
 #define MPORT_MASTER_VERSION 13
 #define MPORT_BUNDLE_VERSION 6
 #define MPORT_BUNDLE_VERSION_STR "6"
-#define MPORT_VERSION "2.7.4"
+#define MPORT_VERSION "2.7.5"
 
 #define MPORT_SETTING_MIRROR_REGION "mirror_region"
 #define MPORT_SETTING_TARGET_OS "target_os"
