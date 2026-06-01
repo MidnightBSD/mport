2caf8edd23d40135f3151a179a9b32cd7caa7722
Lucas Holt <luke@foolishgames.com>
2026-06-01 14:19:26 -0400

    Add HTTP fallback for package fetches

--- libmport/fetch.c
+++ libmport/fetch.c
@@ -42,12 +42,15 @@
 #include <unistd.h>
 
 #define BUFFSIZE 1024 * 8
+#define MPORT_FORCE_HTTP_ENV "MPORT_FORCE_HTTP"
 
 static int fetch(mportInstance *, const char *, const char *);
 static int fetch_bundle_to_dir(mportInstance *, const char *, const char *, const char *);
 static int fetch_to_file(mportInstance *, const char *, FILE *, bool);
+static int force_http_url(/*@null@*/ char **);
 
 static bool is_valid_bundle_filename(const char *);
+static bool force_http_fetch_enabled(void);
 static bool url_is_https(const char *);
 
 static bool
@@ -62,7 +65,6 @@ is_valid_bundle_filename(const char *filename)
 
 	return true;
 }
-
 
 /* mport_fetch_index(mport)
  *
@@ -79,35 +81,47 @@ mport_fetch_index(mportInstance *mport)
 	char *hashUrl = NULL;
 	char *osrel;
 	int mirrorCount = 0;
-	
+
 	MPORT_CHECK_FOR_INDEX(mport, "mport_fetch_index()");
- 
+
 	if (mport_index_get_mirror_list(mport, &mirrors, &mirrorCount) != MPORT_OK)
 		RETURN_CURRENT_ERROR;
 
-#ifdef DEBUG 
+#ifdef DEBUG
 	fprintf(stderr, "Mirror count is %d\n", mirrorCount);
 #endif
- 
+
 	mirrorsPtr = mirrors;
 	osrel = mport_get_osrelease(mport);
-	 
+
 	while (mirrorsPtr != NULL) {
 		if (*mirrorsPtr == NULL)
 			break;
 		if (!url_is_https(*mirrorsPtr)) {
-			mport_call_msg_cb(mport, "Skipping non-HTTPS mirror URL: %s\n", *mirrorsPtr);
+			mport_call_msg_cb(
+			    mport, "Skipping non-HTTPS mirror URL: %s\n", *mirrorsPtr);
 			mirrorsPtr++;
 			continue;
 		}
-		
-		if (asprintf(&url, "%s/%s/%s/%s", *mirrorsPtr,  MPORT_ARCH, osrel, MPORT_INDEX_FILE_SOURCE) == -1 ||
-		    asprintf(&hashUrl, "%s/%s/%s/%s", *mirrorsPtr,  MPORT_ARCH, osrel, MPORT_INDEX_FILE_SOURCE ".sha256") == -1) {
+
+		if (asprintf(&url, "%s/%s/%s/%s", *mirrorsPtr, MPORT_ARCH, osrel,
+			MPORT_INDEX_FILE_SOURCE) == -1 ||
+		    asprintf(&hashUrl, "%s/%s/%s/%s", *mirrorsPtr, MPORT_ARCH, osrel,
+			MPORT_INDEX_FILE_SOURCE ".sha256") == -1) {
 			free(url);
 			free(hashUrl);
 			for (int mi = 0; mi < mirrorCount; mi++)
 				free(mirrors[mi]);
 			RETURN_ERROR(MPORT_ERR_FATAL, "Out of memory.");
+		}
+		if (force_http_url(&url) != MPORT_OK || force_http_url(&hashUrl) != MPORT_OK) {
+			free(url);
+			free(hashUrl);
+			free(osrel);
+			for (int mi = 0; mi < mirrorCount; mi++)
+				free(mirrors[mi]);
+			free(mirrors);
+			RETURN_CURRENT_ERROR;
 		}
 
 		if (fetch(mport, url, MPORT_INDEX_FILE_COMPRESSED) == MPORT_OK) {
@@ -115,13 +129,16 @@ mport_fetch_index(mportInstance *mport)
 				char *hash = mport_extract_hash_from_file(MPORT_INDEX_FILE_HASH);
 				free(hashUrl);
 
-				if (hash == NULL || mport_verify_hash(MPORT_INDEX_FILE_COMPRESSED, hash) == 0) {
-					mport_call_msg_cb(mport, "Index hash failed verification: %s\n", hash);
+				if (hash == NULL ||
+				    mport_verify_hash(MPORT_INDEX_FILE_COMPRESSED, hash) == 0) {
+					mport_call_msg_cb(
+					    mport, "Index hash failed verification: %s\n", hash);
 					free(hash);
 					free(url);
 					continue;
 				} else {
-					if (mport_decompress_zstd(MPORT_INDEX_FILE_COMPRESSED, mport_index_file_path()) != MPORT_OK) {
+					if (mport_decompress_zstd(MPORT_INDEX_FILE_COMPRESSED,
+						mport_index_file_path()) != MPORT_OK) {
 						free(url);
 						free(hash);
 						free(osrel);
@@ -151,19 +168,17 @@ mport_fetch_index(mportInstance *mport)
 	/* fallback to mport bootstrap site in a pinch */
 	if (mport_fetch_bootstrap_index(mport) == MPORT_OK)
 		return MPORT_OK;
-	
-	for (int mi = 0; mi < mirrorCount; mi++) 
+
+	for (int mi = 0; mi < mirrorCount; mi++)
 		free(mirrors[mi]);
 
 	free(mirrors);
 	RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to fetch index file: %s", mport_err_string());
 }
 
-
-
 /* mport_fetch_bootstrap_index(mportInstance *mport)
  *
- * Fetches the index for the bootstrap site. The index need 
+ * Fetches the index for the bootstrap site. The index need
  * not be loaded for this to be used.
  */
 int
@@ -180,12 +195,20 @@ mport_fetch_bootstrap_index(mportInstance *mport)
 		RETURN_ERROR(MPORT_ERR_FATAL, "Bootstrap index URL must use HTTPS");
 	}
 
-	if (asprintf(&url, "%s/%s/%s/%s", MPORT_BOOTSTRAP_INDEX_URL, MPORT_ARCH, osrel, MPORT_INDEX_FILE_SOURCE) == -1 ||
-	    asprintf(&hashUrl, "%s/%s/%s/%s", MPORT_BOOTSTRAP_INDEX_URL,  MPORT_ARCH, osrel, MPORT_INDEX_FILE_SOURCE ".sha256") == -1) {
+	if (asprintf(&url, "%s/%s/%s/%s", MPORT_BOOTSTRAP_INDEX_URL, MPORT_ARCH, osrel,
+		MPORT_INDEX_FILE_SOURCE) == -1 ||
+	    asprintf(&hashUrl, "%s/%s/%s/%s", MPORT_BOOTSTRAP_INDEX_URL, MPORT_ARCH, osrel,
+		MPORT_INDEX_FILE_SOURCE ".sha256") == -1) {
 		free(url);
 		free(hashUrl);
 		free(osrel);
 		return MPORT_ERR_FATAL;
+	}
+	if (force_http_url(&url) != MPORT_OK || force_http_url(&hashUrl) != MPORT_OK) {
+		free(url);
+		free(hashUrl);
+		free(osrel);
+		RETURN_CURRENT_ERROR;
 	}
 
 	result = fetch(mport, url, MPORT_INDEX_FILE_COMPRESSED);
@@ -197,7 +220,8 @@ mport_fetch_bootstrap_index(mportInstance *mport)
 			    hash != NULL ? hash : "(null)");
 			result = MPORT_ERR_FATAL;
 		} else {
-			result = mport_decompress_zstd(MPORT_INDEX_FILE_COMPRESSED, mport_index_file_path());
+			result = mport_decompress_zstd(
+			    MPORT_INDEX_FILE_COMPRESSED, mport_index_file_path());
 		}
 		free(hash);
 	} else {
@@ -214,7 +238,7 @@ mport_fetch_bootstrap_index(mportInstance *mport)
 /* mport_fetch_bundle(mport, filename)
  *
  * Fetch a given bundle from a remote.	If there is no loaded index, then
- * an error is thrown.	The file will be downloaded to the 
+ * an error is thrown.	The file will be downloaded to the
  * MPORT_FETCH_STAGING_DIR directory.
  */
 int
@@ -232,26 +256,35 @@ mport_fetch_bundle(mportInstance *mport, const char *directory, const char *file
 	if (!is_valid_bundle_filename(filename)) {
 		RETURN_ERROR(MPORT_ERR_FATAL, "Invalid bundle filename");
 	}
-	
+
 	if (mport_index_get_mirror_list(mport, &mirrors, &mirrorCount) != MPORT_OK)
 		RETURN_CURRENT_ERROR;
 
 	fetch_dir = directory == NULL ? MPORT_FETCH_STAGING_DIR : directory;
- 
+
 	mirrorsPtr = mirrors;
 	osrel = mport_get_osrelease(mport);
- 
+
 	while (mirrorsPtr != NULL) {
 		if (*mirrorsPtr == NULL)
 			break;
 		if (!url_is_https(*mirrorsPtr)) {
-			mport_call_msg_cb(mport, "Skipping non-HTTPS mirror URL: %s\n", *mirrorsPtr);
+			mport_call_msg_cb(
+			    mport, "Skipping non-HTTPS mirror URL: %s\n", *mirrorsPtr);
 			mirrorsPtr++;
 			continue;
 		}
-		if (asprintf(&url, "%s/%s/%s/%s", *mirrorsPtr,  MPORT_ARCH, osrel, filename) == -1) {
+		if (asprintf(&url, "%s/%s/%s/%s", *mirrorsPtr, MPORT_ARCH, osrel, filename) == -1) {
 			free(osrel);
 			RETURN_ERROR(MPORT_ERR_FATAL, "Out of memory");
+		}
+		if (force_http_url(&url) != MPORT_OK) {
+			free(url);
+			free(osrel);
+			for (int mi = 0; mi < mirrorCount; mi++)
+				free(mirrors[mi]);
+			free(mirrors);
+			RETURN_CURRENT_ERROR;
 		}
 
 		if (fetch_bundle_to_dir(mport, url, fetch_dir, filename) == MPORT_OK) {
@@ -260,8 +293,8 @@ mport_fetch_bundle(mportInstance *mport, const char *directory, const char *file
 			for (int mi = 0; mi < mirrorCount; mi++)
 				free(mirrors[mi]);
 			return MPORT_OK;
-		} 
-		
+		}
+
 		free(url);
 		url = NULL;
 		mirrorsPtr++;
@@ -271,7 +304,34 @@ mport_fetch_bundle(mportInstance *mport, const char *directory, const char *file
 	for (int mi = 0; mi < mirrorCount; mi++)
 		free(mirrors[mi]);
 
-	RETURN_CURRENT_ERROR; 
+	RETURN_CURRENT_ERROR;
+}
+
+static bool
+force_http_fetch_enabled(void)
+{
+	const char *value = getenv(MPORT_FORCE_HTTP_ENV);
+
+	return value != NULL && value[0] != '\0';
+}
+
+static int
+force_http_url(/*@null@*/ char **url_p)
+{
+	char *url;
+
+	if (!force_http_fetch_enabled())
+		return MPORT_OK;
+	if (url_p == NULL || *url_p == NULL || !url_is_https(*url_p))
+		return MPORT_OK;
+
+	if (asprintf(&url, "http://%s", *url_p + 8) == -1)
+		RETURN_ERROR(MPORT_ERR_FATAL, "Out of memory.");
+
+	free(*url_p);
+	*url_p = url;
+
+	return MPORT_OK;
 }
 
 static bool
@@ -282,7 +342,6 @@ url_is_https(const char *url)
 
 	return strncasecmp(url, "https://", 8) == 0;
 }
-
 
 char *
 mport_fetch_cves(mportInstance *mport, char *cpe)
@@ -297,8 +356,10 @@ mport_fetch_cves(mportInstance *mport, char *cpe)
 	if ((fd = mkstemp(tmpfile2)) == -1) {
 		SET_ERRORX(MPORT_ERR_FATAL, "Couldn't make tmp file: %s", strerror(errno));
 	}
-  
-	if (asprintf(&url, "%s/api/cpe/partial-match?includeVersion=true&cpe=%s&startDate=2006-02-28", MPORT_SECURITY_URL, cpe) == -1)
+
+	if (asprintf(&url,
+		"%s/api/cpe/partial-match?includeVersion=true&cpe=%s&startDate=2006-02-28",
+		MPORT_SECURITY_URL, cpe) == -1)
 		return NULL;
 
 	local = fdopen(fd, "w");
@@ -311,27 +372,29 @@ mport_fetch_cves(mportInstance *mport, char *cpe)
 
 	result = fetch_to_file(mport, url, local, false);
 	if (fclose(local) == EOF && result == MPORT_OK)
-		result = SET_ERRORX(MPORT_ERR_FATAL, "Unable to close %s: %s", tmpfile2, strerror(errno));
+		result = SET_ERRORX(
+		    MPORT_ERR_FATAL, "Unable to close %s: %s", tmpfile2, strerror(errno));
 	free(url);
 
-    if (result!= MPORT_OK) {
-       return NULL;
-    }
+	if (result != MPORT_OK) {
+		return NULL;
+	}
 	return strdup(tmpfile2); // return the file path
 }
 
 static int
-fetch(mportInstance *mport, const char *url, const char *dest) 
+fetch(mportInstance *mport, const char *url, const char *dest)
 {
 	FILE *local = NULL;
-	
+
 	if ((local = fopen(dest, "w")) == NULL) {
 		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to open %s: %s", dest, strerror(errno));
 	}
 
 	int result = fetch_to_file(mport, url, local, true);
 	if (fclose(local) == EOF && result == MPORT_OK)
-		result = SET_ERRORX(MPORT_ERR_FATAL, "Unable to close %s: %s", dest, strerror(errno));
+		result =
+		    SET_ERRORX(MPORT_ERR_FATAL, "Unable to close %s: %s", dest, strerror(errno));
 	if (result == MPORT_ERR_FATAL) {
 		unlink(dest);
 	}
@@ -340,8 +403,8 @@ fetch(mportInstance *mport, const char *url, const char *dest)
 }
 
 static int
-fetch_bundle_to_dir(mportInstance *mport, const char *url, const char *directory,
-    const char *filename)
+fetch_bundle_to_dir(
+    mportInstance *mport, const char *url, const char *directory, const char *filename)
 {
 	FILE *local = NULL;
 	int dirfd = -1;
@@ -356,7 +419,8 @@ fetch_bundle_to_dir(mportInstance *mport, const char *url, const char *directory
 	}
 
 	if (mkdir(directory, S_IRWXU | S_IRWXG) == -1 && errno != EEXIST) {
-		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to create %s: %s", directory, strerror(errno));
+		RETURN_ERRORX(
+		    MPORT_ERR_FATAL, "Unable to create %s: %s", directory, strerror(errno));
 	}
 
 	dirfd = open(directory, O_RDONLY | O_DIRECTORY | O_NOFOLLOW | O_CLOEXEC);
@@ -365,40 +429,42 @@ fetch_bundle_to_dir(mportInstance *mport, const char *url, const char *directory
 	}
 
 	for (int i = 0; i < 100; i++) {
-		len = snprintf(tmpname, sizeof(tmpname), ".%s.tmp.%08x", filename,
-		    arc4random());
+		len = snprintf(tmpname, sizeof(tmpname), ".%s.tmp.%08x", filename, arc4random());
 		if (len < 0 || (size_t)len >= sizeof(tmpname)) {
 			close(dirfd);
 			RETURN_ERROR(MPORT_ERR_FATAL, "Bundle filename is too long");
 		}
 
-		fd = openat(dirfd, tmpname,
-		    O_WRONLY | O_CREAT | O_EXCL | O_NOFOLLOW | O_CLOEXEC,
+		fd = openat(dirfd, tmpname, O_WRONLY | O_CREAT | O_EXCL | O_NOFOLLOW | O_CLOEXEC,
 		    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 		if (fd != -1)
 			break;
 		if (errno != EEXIST) {
 			close(dirfd);
-			RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to open %s/%s: %s", directory, tmpname, strerror(errno));
+			RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to open %s/%s: %s", directory,
+			    tmpname, strerror(errno));
 		}
 	}
 
 	if (fd == -1) {
 		close(dirfd);
-		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to create temporary bundle file in %s", directory);
+		RETURN_ERRORX(
+		    MPORT_ERR_FATAL, "Unable to create temporary bundle file in %s", directory);
 	}
 
 	if (fstat(fd, &sb) == -1) {
 		close(fd);
 		unlinkat(dirfd, tmpname, 0);
 		close(dirfd);
-		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to stat %s/%s: %s", directory, tmpname, strerror(errno));
+		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to stat %s/%s: %s", directory, tmpname,
+		    strerror(errno));
 	}
 	if (!S_ISREG(sb.st_mode) || sb.st_nlink != 1) {
 		close(fd);
 		unlinkat(dirfd, tmpname, 0);
 		close(dirfd);
-		RETURN_ERRORX(MPORT_ERR_FATAL, "Refusing unsafe bundle destination %s/%s", directory, tmpname);
+		RETURN_ERRORX(MPORT_ERR_FATAL, "Refusing unsafe bundle destination %s/%s",
+		    directory, tmpname);
 	}
 
 	local = fdopen(fd, "w");
@@ -406,13 +472,15 @@ fetch_bundle_to_dir(mportInstance *mport, const char *url, const char *directory
 		close(fd);
 		unlinkat(dirfd, tmpname, 0);
 		close(dirfd);
-		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to open %s/%s: %s", directory, tmpname, strerror(errno));
+		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to open %s/%s: %s", directory, tmpname,
+		    strerror(errno));
 	}
 	fd = -1;
 
 	result = fetch_to_file(mport, url, local, true);
 	if (fclose(local) == EOF && result == MPORT_OK)
-		result = SET_ERRORX(MPORT_ERR_FATAL, "Unable to close %s/%s: %s", directory, tmpname, strerror(errno));
+		result = SET_ERRORX(MPORT_ERR_FATAL, "Unable to close %s/%s: %s", directory,
+		    tmpname, strerror(errno));
 	if (result == MPORT_ERR_FATAL) {
 		unlinkat(dirfd, tmpname, 0);
 		close(dirfd);
@@ -422,8 +490,8 @@ fetch_bundle_to_dir(mportInstance *mport, const char *url, const char *directory
 	if (renameat(dirfd, tmpname, dirfd, filename) == -1) {
 		unlinkat(dirfd, tmpname, 0);
 		close(dirfd);
-		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to rename %s/%s to %s/%s: %s",
-		    directory, tmpname, directory, filename, strerror(errno));
+		RETURN_ERRORX(MPORT_ERR_FATAL, "Unable to rename %s/%s to %s/%s: %s", directory,
+		    tmpname, directory, filename, strerror(errno));
 	}
 
 	close(dirfd);
@@ -431,50 +499,51 @@ fetch_bundle_to_dir(mportInstance *mport, const char *url, const char *directory
 }
 
 /* Copies url into local. The caller owns local and must fclose it. */
-static int 
-fetch_to_file(mportInstance *mport, const char *url, FILE *local, bool progress) 
+static int
+fetch_to_file(mportInstance *mport, const char *url, FILE *local, bool progress)
 {
 	FILE *remote = NULL;
 	struct url_stat ustat;
 	char buffer[BUFFSIZE];
 	char *ptr = NULL;
-	size_t size;																	
+	size_t size;
 	size_t got = 0;
 	size_t wrote;
 	char msg[1024];
 
-	if (progress)	
+	if (progress)
 		mport_call_progress_init_cb(mport, "Downloading %s", url);
-	
+
 	if ((remote = fetchXGetURL(url, &ustat, "p")) == NULL) {
 		RETURN_ERRORX(MPORT_ERR_FATAL, "Fetch error: %s: %s", url, fetchLastErrString);
 	}
 
 	char pkg[128];
 	char *loc = strrchr(url, '/');
-	if (loc!=NULL) {
+	if (loc != NULL) {
 		strlcpy(pkg, loc + 1, 127);
 	} else {
 		strlcpy(pkg, url, 127);
 	}
 	double dlpercent = 0.0;
-	
+
 	while (1) {
 		size = fread(buffer, 1, BUFFSIZE, remote);
-		
+
 		if (size < BUFFSIZE) {
 			if (ferror(remote)) {
 				fclose(remote);
-				RETURN_ERRORX(MPORT_ERR_FATAL, "Fetch error: %s: %s", url, fetchLastErrString);	 
+				RETURN_ERRORX(MPORT_ERR_FATAL, "Fetch error: %s: %s", url,
+				    fetchLastErrString);
 			} else if (feof(remote)) {
 				/* do nothing */
-			} 
-		} 
-	
+			}
+		}
+
 		got += size;
 
-		if (progress) {	
-			double val = ((double)got / (double) ustat.size) * 100;
+		if (progress) {
+			double val = ((double)got / (double)ustat.size) * 100;
 			if (val > dlpercent) {
 				dlpercent = val;
 				snprintf(msg, 1024, "Downloading %s (%.2f%%)", pkg, dlpercent);
@@ -493,7 +562,7 @@ fetch_to_file(mportInstance *mport, const char *url, FILE *local, bool progress)
 		if (feof(remote))
 			break;
 	}
-	
+
 	fclose(remote);
 
 	if (progress)
@@ -510,7 +579,9 @@ fetch_to_file(mportInstance *mport, const char *url, FILE *local, bool progress)
  * @param path returns the path of the saved file. Must be freed on success
  */
 int
-mport_download(mportInstance *mport, const char *packageName, bool all, bool includeDependencies, char **path) {
+mport_download(
+    mportInstance *mport, const char *packageName, bool all, bool includeDependencies, char **path)
+{
 	mportIndexEntry **indexEntries = NULL;
 	mportIndexEntry *indexEntry = NULL;
 	mportDependsEntry **depends = NULL;
@@ -525,7 +596,8 @@ mport_download(mportInstance *mport, const char *packageName, bool all, bool inc
 
 		while (*ie2 != NULL) {
 			char *dpath;
-			if (mport_download(mport, (*ie2)->pkgname, false, false, &dpath) != MPORT_OK) {
+			if (mport_download(mport, (*ie2)->pkgname, false, false, &dpath) !=
+			    MPORT_OK) {
 				mport_call_msg_cb(mport, "%s", mport_err_string());
 				mport_index_entry_free_vec(ie2_orig);
 				return mport_err_code();
@@ -537,11 +609,11 @@ mport_download(mportInstance *mport, const char *packageName, bool all, bool inc
 		return (MPORT_OK);
 	}
 
-	if (mport_index_select_pkgname(mport, packageName, "Multiple packages match your query.", &indexEntries, &indexEntry) !=
-	    MPORT_OK) {
+	if (mport_index_select_pkgname(mport, packageName, "Multiple packages match your query.",
+		&indexEntries, &indexEntry) != MPORT_OK) {
 		RETURN_CURRENT_ERROR;
 	}
-	
+
 	if (indexEntry == NULL) {
 		SET_ERRORX(1, "Package %s not found in index.\n", packageName);
 		mport_index_entry_free_vec(indexEntries);
@@ -559,7 +631,7 @@ mport_download(mportInstance *mport, const char *packageName, bool all, bool inc
 		mport_index_entry_free_vec(indexEntries);
 		RETURN_CURRENT_ERROR;
 	}
-	
+
 	if (asprintf(path, "%s/%s", mport->outputPath, indexEntry->bundlefile) == -1) {
 		mport_index_entry_free_vec(indexEntries);
 		indexEntries = NULL;
@@ -569,14 +641,16 @@ mport_download(mportInstance *mport, const char *packageName, bool all, bool inc
 	}
 
 	if (includeDependencies) {
-		mport_index_depends_list(mport, indexEntry->pkgname, indexEntry->version, &depends_orig);
+		mport_index_depends_list(
+		    mport, indexEntry->pkgname, indexEntry->version, &depends_orig);
 		depends = depends_orig;
 
 		while (*depends != NULL) {
 			char *dpath;
-			if (mport_download(mport, (*depends)->d_pkgname, false, includeDependencies, &dpath) != MPORT_OK) {
-     			mport_call_msg_cb(mport, "%s", mport_err_string());
-     			mport_index_depends_free_vec(depends_orig);
+			if (mport_download(mport, (*depends)->d_pkgname, false, includeDependencies,
+				&dpath) != MPORT_OK) {
+				mport_call_msg_cb(mport, "%s", mport_err_string());
+				mport_index_depends_free_vec(depends_orig);
 				return mport_err_code();
 			}
 			free(dpath);
@@ -587,22 +661,23 @@ mport_download(mportInstance *mport, const char *packageName, bool all, bool inc
 
 getfile:
 	if (!mport_file_exists(*path)) {
-		if (mport_fetch_bundle(mport, mport->outputPath, indexEntry->bundlefile) != MPORT_OK) {
-			mport_call_msg_cb(mport, "Error fetching package %s, %s", packageName, mport_err_string());
+		if (mport_fetch_bundle(mport, mport->outputPath, indexEntry->bundlefile) !=
+		    MPORT_OK) {
+			mport_call_msg_cb(mport, "Error fetching package %s, %s", packageName,
+			    mport_err_string());
 			free(*path);
 			path = NULL;
 			mport_index_entry_free_vec(indexEntries);
 			indexEntries = NULL;
 			indexEntry = NULL;
 			return mport_err_code();
-			
 		}
 		existed = false;
 	}
 
 	if (!mport_verify_hash(*path, indexEntry->hash)) {
 		if (existed) {
-			if (unlink(*path) == 0)	{
+			if (unlink(*path) == 0) {
 				retryCount++;
 
 				if (retryCount < 2)
@@ -610,7 +685,8 @@ getfile:
 			}
 		} else {
 			/* the index might be stale. re-fetch it */
-			mport_call_msg_cb(mport, "The index might be stale. Attempting to refresh it.\n");
+			mport_call_msg_cb(
+			    mport, "The index might be stale. Attempting to refresh it.\n");
 			mport_index_get(mport);
 			retryCount++;
 			if (retryCount < 2)
